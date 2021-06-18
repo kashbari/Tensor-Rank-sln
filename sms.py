21a8e0147d62ffad2648227b4b5a3e1df9e89b13
kashbari <kashbari@gmail.com>
2021-06-18 16:41:00 -0700

    added rank/linbox files

---
+++ sms.py
@@ -0,0 +1,17 @@
+import scipy.sparse
+
+def tosms(m):
+	I,J,V = scipy.sparse.find(m)
+	out = '%d %d M\n' % m.shape
+	out += ''.join(['%d %d %d\n' %(i+1,j+1,v) for i,j,v in zip(I,J,V)]) + '0 0 0\n'
+	return out
+
+# m is scipy any sparse format
+def linbox_rank(m):
+	import subprocess
+	proc = subprocess.Popen(['/home/kbari/git_repo/LinBoxPython/rank'],stdin=subprocess.PIPE,stdout=subprocess.PIPE,shell=True)
+	proc.stdin.write(tosms(m).encode())
+	return int(proc.communicate()[0])
+
+
+
